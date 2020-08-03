89759ff71bcfde93e254030a544ad7380ba6db16
Prabhjot Singh <prince112592@yahoo.com>
2020-08-03 16:52:23 -0700

    Added proper snake trail with food collision

--- snake.js
+++ snake.js
@@ -1,80 +1,119 @@
-var posx;
-var posy;
-var grid;
+const GRID_WIDTH = GRID_HEIGHT = 30;
+const ROWS = COLS = 20;
+const DEBUG = true;
+
+var foodX;
+var foodY;
 var hasFood;
 var direction;
-var numRect;
+var snake;
+var snakeLength;
 var speed; 
+var inGame;
+var pause;
 
 
 function setup() {
-  createCanvas(600,600);
-  //noStroke();
+  createCanvas(COLS * GRID_WIDTH, ROWS * GRID_HEIGHT);
+  frameRate(5);  
+  // noStroke();
  
-  posx = 0;
-  posy = 0;
   hasFood = false;
-  numRect = 1;
+  snake = [{x: 0, y: 0}];
+  snakeLength = 1;
   speed = 1;
-  grid = Create2DArray(20);
-  frameRate(5);
-  console.log(grid)
+  inGame = true;
+  pause = false;
   
-  
-  for (var i = 0; i < 20; i++)
-    for (var j = 0; j < 20; j++) {
-      grid[i][j] = false;
+  if (DEBUG) {
+    // Grid lines
+    for(var i = 0; i <= width; i += GRID_WIDTH) {
+      line(i, 0, i, height);
+      line(0, i, width, i);
     }
+  }
 }
-
 
 
 function draw() {
-  
-  for(var i = 0; i < width; i+=30) {
-   line(i, 0, i, height);
-   line(0, i, width, i);
-    
-  }
-  
-  drawRects();
-  spawnFood();
-  
-  switch(direction) {
-    case 'w': posy--; break;
-    case 's': posy++; break;
-    case 'a': posx--; break;
-    case 'd': posx++; break;
+  if (DEBUG) {
+
   }
 
-  grid[posx][posy] = true;
+  if (pause) {
+    textSize(50);
+    textAlign(CENTER);
+    text("PAUSE", (COLS / 2) * GRID_WIDTH, (ROWS / 2) * GRID_HEIGHT);
+  } else if (inGame) {
+    // Move
+    let {x, y} = snake[0]
+    switch(direction) {
+      case 'w': y--; break;
+      case 's': y++; break;
+      case 'a': x--; break;
+      case 'd': x++; break;
+    }
+    snake.unshift({x: x, y: y}); // Add the new head to the front
+    
+    drawScore();
+    checkFood();
+    checkCollision();
+    drawTrail();
+    spawnFood();
+  }
+  else
+    gameOver();
 }
 
 
+function drawScore() {
+  textSize(10);
+  text("Score: " + (snakeLength - 1).toString(), (COLS) * GRID_WIDTH - 30, (ROWS) * GRID_HEIGHT - 30);
+}
 
-function drawRects() {
-  for (var i = 0; i < 20; i++)
-    for (var j = 0; j < 20; j++) {
-      if (grid[i][j]) {
-        fill(255, 0, 0, 100);
-        rect(i * 30, j * 30, 30, 30);
-        fill(0);
-      }
-    }
+
+function checkFood() {
+  if (snake[0].x === foodX && snake[0].y  === foodY) {
+    snakeLength += 5;
+    hasFood = false;
+  }
+}
+
+
+function checkCollision() {
+
+}
+
+
+function drawTrail() {
+  // Fill the old squares
+  for (let i = snakeLength; i < snake.length; i++) {
+    let pos = snake[i]
+    fill(255)
+    rect(pos.x * GRID_WIDTH, pos.y * GRID_HEIGHT, GRID_WIDTH, GRID_HEIGHT);
+    fill(0);
+  }
+  snake = snake.slice(0, snakeLength);  // Enforce the snake's length
+
+  // Fill the new squares
+  for (let i = 0; i < snake.length; i++) {
+    pos = snake[i]
+    fill(255, 0, 0, 100);
+    rect(pos.x * GRID_WIDTH, pos.y * GRID_HEIGHT, GRID_WIDTH, GRID_HEIGHT);
+    fill(0);
+  }
 }
 
 
 function spawnFood() {
-  
   if (!hasFood) {
-    var foodX = randomNumber(0, 20);
-    var foodY = randomNumber(0, 20);
+    foodX = randomNumber(0, COLS);
+    foodY = randomNumber(0, ROWS);
     fill(0, 255, 0, 100);
-    rect(foodX * 30, foodY * 30, 30, 30);
+    rect(foodX * GRID_WIDTH, foodY * GRID_HEIGHT, GRID_WIDTH, GRID_HEIGHT);
     fill(0);
     hasFood = true;
   }
-  
 }
 
 
@@ -84,23 +123,21 @@ function keyPressed() {
     case 's': direction = 's'; break;
     case 'a': direction = 'a'; break;
     case 'd': direction = 'd'; break;
+    case 'p': pause = !pause; break;
   }
-  
 }
 
 
-// Helper Functions
-
-function Create2DArray(rows) {
-  var arr = [];
-
-  for (var i=0;i<rows;i++) {
-     arr[i] = [];
-  }
-
-  return arr;
+function gameOver() {
+  textSize(50);
+  textAlign(CENTER);
+  text("Game over!", (COLS / 2) * GRID_WIDTH, (ROWS / 2) * GRID_HEIGHT);
+  textSize(30);
+  text("Score: " + (snakeLength - 1).toString(), (COLS / 2) * GRID_WIDTH, (ROWS / 2) * GRID_HEIGHT + 30);
 }
 
+
+// Return int between min and max: min included, max excluded
 function randomNumber(min, max) {
-  return Math.random() * (max - min) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
